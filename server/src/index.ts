064afd880a1f1319990af0f58c0e7f64c28b0851
Sergio Sampaio <sergiosampaio@Sergios-MacBook-Pro.local>
2021-03-19 00:39:46 -0300

    Added graphql with some crud methods

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,10 +1,32 @@
+import "reflect-metadata";
 import { MikroORM } from '@mikro-orm/core';
-import { __prod__ } from './constants';
+import { __port__, __prod__ } from './constants';
 import mikroConfig from './mikro-orm.config';
+import express from 'express';
+import { ApolloServer } from 'apollo-server-express';
+import { buildSchema } from 'type-graphql';
+import { HelloResolver } from './resolvers/hello';
+import { PostResolver } from './resolvers/post';
 
 const main = async () => {
   const orm = await MikroORM.init(mikroConfig);
   await orm.getMigrator().up();
+  
+  const app = express();
+
+  const apolloServer = new ApolloServer({
+    schema: await buildSchema({
+      resolvers: [HelloResolver, PostResolver],
+      validate: false,
+    }),
+    context: () => ({ em: orm.em }),
+  })
+
+  apolloServer.applyMiddleware({ app });
+
+  app.listen(__port__, () => {
+    console.log(`Server started on localhost: ${__port__}`)
+  });
 }
 
 main().catch((err) => console.log(err.message));
